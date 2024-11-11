4e67e10016e413840d74971d41351598b7532976
mzubair21 <mzubair21@outlook.com>
2024-11-11 07:22:17 +0500

    responsive: Clear Eats in Action

--- src/app/components/Action.tsx
+++ src/app/components/Action.tsx
@@ -1,9 +1,16 @@
 "use client"
 
-import AOS from "aos"
 import "aos/dist/aos.css"
+import "swiper/css"
+import "swiper/css/navigation"
+import "swiper/css/effect-coverflow"
+
+import { useEffect } from "react"
+import AOS from "aos"
 import Image from "next/image"
-import { FC, useEffect, useState } from "react"
+import { Swiper, SwiperSlide } from "swiper/react"
+import { Navigation, Autoplay } from "swiper/modules"
+
 import actions1 from "../assets/images/action1.png"
 import actions2 from "../assets/images/action2.png"
 import actions3 from "../assets/images/action3.png"
@@ -11,27 +18,12 @@ import actionBg from "../assets/svgs/landingpage/Action-bg.svg"
 import leftArrow from "../assets/svgs/landingpage/subscriptionleftarrow.svg"
 import rightArrow from "../assets/svgs/landingpage/subscriptionrightarrow.svg"
 
-const Action: FC = () => {
+const Action = () => {
   useEffect(() => {
     AOS.init({ duration: 1200 })
   }, [])
-  const images = [actions1, actions2, actions3, actions2, actions1, actions2]
 
-  const [currentIndex, setCurrentIndex] = useState(0)
-
-  const nextSlide = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex + 3 >= images.length ? 0 : prevIndex + 3
-    )
-  }
-
-  const prevSlide = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === 0 ? images.length - 3 : prevIndex - 3
-    )
-  }
-
-  const currentImages = images.slice(currentIndex, currentIndex + 3)
+  const images = [actions1, actions2, actions3, actions1, actions2, actions3]
 
   return (
     <div
@@ -46,57 +38,76 @@ const Action: FC = () => {
         <h1 className="text-2xl sm:text-3.5xl font-bold mt-4">
           See Clear Eats in Action
         </h1>
-        <p className="mt-4 text-[14px]">
+        <p className="mt-4 text-[14px] text-ssm sm:text-base">
           From managing orders to customizing menus, our platform is built to
           make your life easier and your customers' experiences better.
         </p>
       </div>
 
-      {/* Image carousel section */}
-      <div className="relative flex items-center justify-center">
-        <button
-          className={`absolute left-0 sm:left-6 md:left-8 lg:left-10 top-1/2 transform -translate-y-1/2 z-10`} // Apply hover effect class
-          onClick={prevSlide}
-        >
+      {/* Swiper centered slider */}
+      <div
+        className="relative flex items-center justify-center"
+        data-aos="fade-up"
+      >
+        <button className="absolute left-0 sm:left-6 md:left-8 lg:left-10 top-1/2 transform -translate-y-1/2 z-10">
           <Image
             src={leftArrow}
             alt="Previous"
-            className="w-16 sm:w-24 md:w-28 lg:w-40"
+            className="swiper_prev w-16 sm:w-24 md:w-28 lg:w-40"
             width={100}
             height={100}
           />
         </button>
-        <div
-          className="flex flex-row -space-x-12 sm:-space-x-20 lg:-space-x-24"
-          data-aos="fade-up"
+
+        <Swiper
+          modules={[Navigation, Autoplay]}
+          slidesPerView={3}
+          centeredSlides={true}
+          spaceBetween={0}
+          loop={true}
+          autoplay={{ delay: 2500 }}
+          grabCursor={true}
+          navigation={{
+            nextEl: ".swiper_next",
+            prevEl: ".swiper_prev",
+          }}
+          breakpoints={{
+            0: {
+              slidesPerView: 3,
+              spaceBetween: 0,
+            },
+            520: {
+              slidesPerView: 3,
+              spaceBetween: 0,
+            },
+            1024: {
+              slidesPerView: 3,
+              spaceBetween: 0,
+            },
+          }}
+          className="CenterZoomSlider w-full  max-w-[80%]"
         >
-          {currentImages.map((image, index) => (
-            <div
+          {images.map((image, index) => (
+            <SwiperSlide
               key={index}
-              className={`flex-none transition-transform duration-500 ease-in-out ${
-                index === 1
-                  ? "transform scale-110 translate-y-[-30px]"
-                  : "transform scale-95"
-              }`}
+              className="flex justify-center items-center swiper-slide-custom"
             >
               <Image
                 src={image}
-                className="inline-block w-[150px] sm:w-[250px] lg:w-[370px]"
-                alt={`Slide ${currentIndex + index + 1}`}
-                width={370} // Adjust width as needed
-                height={250} // Adjust height as needed
+                alt={`Slide ${index + 1}`}
+                className="w-[150px] sm:w-[250px] lg:w-[370px]"
+                width={370}
+                height={250}
               />
-            </div>
+            </SwiperSlide>
           ))}
-        </div>
-        <button
-          className={`absolute right-0 sm:right-6 md:right-8 lg:right-10 top-1/2 transform -translate-y-1/2 z-10`} // Apply hover effect class
-          onClick={nextSlide}
-        >
+        </Swiper>
+
+        <button className="absolute right-0 sm:right-6 md:right-8 lg:right-10 top-1/2 transform -translate-y-1/2 z-10">
           <Image
             src={rightArrow}
             alt="Next"
-            className="w-16 sm:w-24 md:w-28 lg:w-40"
+            className="swiper_next w-16 sm:w-24 md:w-28 lg:w-40"
             width={100}
             height={100}
           />
