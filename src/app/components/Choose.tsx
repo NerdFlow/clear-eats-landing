9daeb642e9df5d4705366fe76eec26d39e353112
mzubair21 <mzubair21@outlook.com>
2024-11-11 06:32:10 +0500

    reponsive: choice

--- src/app/components/Choose.tsx
+++ src/app/components/Choose.tsx
@@ -45,61 +45,69 @@ const Choose: FC = () => {
   }
 
   return (
-    <div
-      className="relative flex flex-col lg:flex-row-reverse items-center justify-between lg:justify-around p-6 sm:p-10"
-      id="Features"
-      style={{
-        backgroundImage: `url(${chooseBg.src})`,
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover", // Make sure the background covers the container
-        overflow: "hidden", // Prevent overflow issues
-      }}
-    >
-      {/* Image Section */}
-      <div className="flex-none mt-2 md:mt-0" data-aos="fade-left">
-        <Image
-          src={chooseImage}
-          alt="Choose"
-          className="w-[300px] sm:w-[400px] md:w-[500px] lg:w-[450px] ml-0 md:ml-4"
-        />
-      </div>
-
-      {/* Text & Carousel Section */}
+    <section>
       <div
-        className="flex-none max-w-full lg:max-w-[500px]"
-        data-aos="fade-right"
+        className=""
+        id="Features"
+        style={{
+          backgroundImage: `url(${chooseBg.src})`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover", // Make sure the background covers the container
+          overflow: "hidden", // Prevent overflow issues
+        }}
       >
-        <h1
-          className="font-bold text-center text-2xl sm:text-3xl  text-black-lightblack"
-          style={{ lineHeight: "1.3" }}
-        >
-          Why Clear Eats is the Right Choice for Your Restaurant
-        </h1>
-        <p className="mb-6 mt-4 text-center lg:text-left text-xs lg:text-sm">
-          Delivering Performance, Customization, and Speed
-        </p>
+        <div className="container relative flex flex-col gap-4 lg:gap-0 lg:flex-row-reverse items-center justify-between lg:justify-around pt-6 p-12">
+          {/* Image Section */}
+          <div className="flex-none  " data-aos="fade-left">
+            <Image
+              src={chooseImage}
+              alt="Choose"
+              className="w-[300px] sm:w-[400px] md:w-[500px] lg:w-[450px] ml-0 md:ml-4"
+            />
+          </div>
 
-        {/* Carousel */}
-        <div className="relative rounded-xl border border-purple bg-lightgrey w-full sm:max-w-[350px] md:max-w-[500px] h-[200px] p-4 lg:p-10 mx-auto">
-          <h4 className="font-bold text-md mb-2">
-            {carouselContent[currentIndex].title}
-          </h4>
-          <p className="leading-6 w-full text-sm">
-            {carouselContent[currentIndex].description}
-          </p>
+          {/* Text & Carousel Section */}
+          <div
+            className="flex-none max-w-full lg:max-w-[500px]"
+            data-aos="fade-right"
+          >
+            <h1
+              className="font-bold text-center lg:text-left text-xl sm:text-2xl xl:text-2xl  text-black-lightblack"
+              style={{ lineHeight: "1.3" }}
+            >
+              Why Clear Eats is the Right Choice for Your Restaurant
+            </h1>
+            <p className="mb-6 mt-4 text-center lg:text-left text-xs lg:text-sm  ">
+              Delivering Performance, Customization, and Speed
+            </p>
 
-          {/* Carousel Navigation */}
-          <div className="absolute bottom-4 right-4 flex space-x-2">
-            <button onClick={prevSlide}>
-              <Image src={leftArrow} alt="Previous" className="w-6 sm:w-8" />
-            </button>
-            <button onClick={nextSlide}>
-              <Image src={rightArrow} alt="Next" className="w-6 sm:w-8" />
-            </button>
+            {/* Carousel */}
+            <div className="relative rounded-xl border border-purple bg-lightgrey w-full  p-4 lg:p-6 lg:pb-12 mx-auto pb-12">
+              <h4 className="font-bold text-md mb-2">
+                {carouselContent[currentIndex].title}
+              </h4>
+              <p className="leading-6 w-full text-sm">
+                {carouselContent[currentIndex].description}
+              </p>
+
+              {/* Carousel Navigation */}
+              <div className="absolute bottom-4 right-4 flex space-x-2">
+                <button onClick={prevSlide}>
+                  <Image
+                    src={leftArrow}
+                    alt="Previous"
+                    className="w-6 sm:w-8"
+                  />
+                </button>
+                <button onClick={nextSlide}>
+                  <Image src={rightArrow} alt="Next" className="w-6 sm:w-8" />
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   )
 }
 
--- src/app/components/Solutions.tsx
+++ src/app/components/Solutions.tsx
@@ -22,143 +22,149 @@ const Solutions: FC = () => {
       className=" font-poppins max-w-full  overflow-x-hidden pt-36 sm:pt-[20%] md:pt-[16%] lg:pt-[20%] xl:pt-[12%] "
       id="about"
     >
-      <div className="text-center p-4 sm:p-12" data-aos="fade-up">
-        <h1 className="font-bold text-2xl sm:text-3xl xl:text-3.5xl text-black-greyish">
-          The Solutions We Offer
-        </h1>
-        <p className="font-normal  text-ssm sm:text-sm ">
-          Three Powerful Tools to Elevate Your Restaurant Experience
-        </p>
-      </div>
-
-      <div className="flex flex-col  lg:flex-row justify-center lg:justify-evenly items-start space-y-8 sm:space-y-6 md:space-y-8 lg:space-y-0 p-2">
-        <div
-          className="w-full  lg:w-[17.5rem] text-center flex flex-col items-center mb-8 sm:mb-6 md:mb-8 lg:mb-0"
-          data-aos="fade-up"
-        >
-          <Image
-            src={allerguard}
-            width={64}
-            height={64}
-            className="mb-4"
-            alt="Allerguard"
-          />
-          <h1 className="font-bold text-xl text-black-greyish sm:text-xl md:text-2xl lg:text-2xl">
-            Allerguard
+      <div className="container">
+        <div className="text-center p-4 sm:p-12" data-aos="fade-up">
+          <h1 className="font-bold text-2xl sm:text-3xl xl:text-3.5xl text-black-greyish">
+            The Solutions We Offer
           </h1>
-          <h5 className="text-black-lightgreyish font-medium text-[12px] mb-2 sm:text-sm md:text-base lg:text-[14px]">
-            Protect Your Customers and Your Brand
-          </h5>
-          <p
-            className="text-xs text-gray-500 sm:text-xs md:text-sm lg:text-xs"
-            style={{ lineHeight: "1.7" }}
-          >
-            Ensure that your menu meets the highest safety standards with our
-            advanced allergen management system.
+          <p className="font-normal  text-ssm sm:text-sm ">
+            Three Powerful Tools to Elevate Your Restaurant Experience
           </p>
         </div>
 
-        <Image src={line} className="hidden  md:block" alt="Divider" />
-
-        <div
-          className="w-full  lg:w-[17.5rem] text-center flex flex-col items-center mb-8 sm:mb-6 md:mb-8 lg:mb-0"
-          data-aos="fade-up"
-        >
-          <Image
-            src={management}
-            width={64}
-            height={64}
-            className="mb-4"
-            alt="Management System"
-          />
-          <h1 className="font-bold text-xl text-black-greyish sm:text-xl md:text-2xl lg:text-2xl">
-            Management System
-          </h1>
-          <h5 className="text-black-lightgreyish font-medium text-[12px] mb-2 sm:text-sm md:text-base lg:text-[14px]">
-            Efficiency at Your Fingertips
-          </h5>
-          <p
-            className="text-xs text-gray-500 sm:text-xs md:text-sm lg:text-xs"
-            style={{ lineHeight: "1.7" }}
-          >
-            Manage orders, track sales, and monitor staff performance with our
-            comprehensive management system. Keep your operations organized and
-            running smoothly, all from one intuitive dashboard.
-          </p>
-        </div>
-
-        <Image src={line} className="hidden  md:block" alt="Divider" />
-
-        <div
-          className="w-full  lg:w-[17.5rem] text-center flex flex-col items-center md:mt-6 lg:mt-0"
-          data-aos="fade-up"
-        >
-          <Image
-            src={loyalty}
-            width={64}
-            height={64}
-            className="mb-4"
-            alt="Loyalty Program"
-          />
-          <h1 className="font-bold text-xl text-black-greyish sm:text-xl md:text-2xl lg:text-2xl">
-            Loyalty Program
-          </h1>
-          <h5 className="text-black-lightgreyish font-medium text-[12px] mb-2 sm:text-sm md:text-base lg:text-[14px]">
-            Build Lasting Relationships
-          </h5>
-          <p
-            className="text-xs text-gray-500 sm:text-xs md:text-sm lg:text-xs"
-            style={{ lineHeight: "1.7" }}
-          >
-            Our flexible loyalty program is designed to keep your customers
-            coming back for more, helping you boost revenue and enhance customer
-            satisfaction.
-          </p>
-        </div>
-      </div>
-      <div className="bg-[#F9F7FE]">
-        <div
-          className="bg-cover flex flex-col lg:flex-row items-center justify-center md:justify-between lg:justify-around p-6 sm:p-8 md:p-12 lg:p-20 mt-16 sm:mt-16 md:mt-18 lg:mt-20 gap-10 xl:gap-0"
-          style={{ backgroundImage: bgImageSolutionUrl }}
-        >
+        <div className="flex flex-col  lg:flex-row justify-center lg:justify-evenly items-start space-y-8 sm:space-y-6 md:space-y-8 lg:space-y-0 p-2">
           <div
-            className="flex-none mb-2 sm:mb-6 md:mb-7 lg:mb-4"
-            data-aos="fade-right"
+            className="w-full  lg:w-[17.5rem] text-center flex flex-col items-center mb-8 sm:mb-6 md:mb-8 lg:mb-0"
+            data-aos="fade-up"
           >
             <Image
-              src={solution2}
-              width={360}
-              height={360}
-              className="w-full sm:w-[360px] md:w-[550px] lg:w-[570px] h-auto"
-              alt="Solutions Overview"
+              src={allerguard}
+              width={64}
+              height={64}
+              className="mb-4"
+              alt="Allerguard"
             />
+            <h1 className="font-bold text-xl text-black-greyish sm:text-xl md:text-2xl lg:text-2xl">
+              Allerguard
+            </h1>
+            <h5 className="text-black-lightgreyish font-medium text-[12px] mb-2 sm:text-sm md:text-base lg:text-[14px]">
+              Protect Your Customers and Your Brand
+            </h5>
+            <p
+              className="text-xs text-gray-500 sm:text-xs md:text-sm lg:text-xs"
+              style={{ lineHeight: "1.7" }}
+            >
+              Ensure that your menu meets the highest safety standards with our
+              advanced allergen management system.
+            </p>
           </div>
+
+          <Image src={line} className="hidden  md:block" alt="Divider" />
+
           <div
-            className="flex-none max-w-[600px] text-center
+            className="w-full  lg:w-[17.5rem] text-center flex flex-col items-center mb-8 sm:mb-6 md:mb-8 lg:mb-0"
+            data-aos="fade-up"
+          >
+            <Image
+              src={management}
+              width={64}
+              height={64}
+              className="mb-4"
+              alt="Management System"
+            />
+            <h1 className="font-bold text-xl text-black-greyish sm:text-xl md:text-2xl lg:text-2xl">
+              Management System
+            </h1>
+            <h5 className="text-black-lightgreyish font-medium text-[12px] mb-2 sm:text-sm md:text-base lg:text-[14px]">
+              Efficiency at Your Fingertips
+            </h5>
+            <p
+              className="text-xs text-gray-500 sm:text-xs md:text-sm lg:text-xs"
+              style={{ lineHeight: "1.7" }}
+            >
+              Manage orders, track sales, and monitor staff performance with our
+              comprehensive management system. Keep your operations organized
+              and running smoothly, all from one intuitive dashboard.
+            </p>
+          </div>
+
+          <Image src={line} className="hidden  md:block" alt="Divider" />
+
+          <div
+            className="w-full  lg:w-[17.5rem] text-center flex flex-col items-center md:mt-6 lg:mt-0"
+            data-aos="fade-up"
+          >
+            <Image
+              src={loyalty}
+              width={64}
+              height={64}
+              className="mb-4"
+              alt="Loyalty Program"
+            />
+            <h1 className="font-bold text-xl text-black-greyish sm:text-xl md:text-2xl lg:text-2xl">
+              Loyalty Program
+            </h1>
+            <h5 className="text-black-lightgreyish font-medium text-[12px] mb-2 sm:text-sm md:text-base lg:text-[14px]">
+              Build Lasting Relationships
+            </h5>
+            <p
+              className="text-xs text-gray-500 sm:text-xs md:text-sm lg:text-xs"
+              style={{ lineHeight: "1.7" }}
+            >
+              Our flexible loyalty program is designed to keep your customers
+              coming back for more, helping you boost revenue and enhance
+              customer satisfaction.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* Empower Section */}
+      <section className="bg-[#F9F7FE]">
+        <div
+          className="bg-cover"
+          style={{ backgroundImage: bgImageSolutionUrl }}
+        >
+          <div className="container flex flex-col lg:flex-row items-center justify-center md:justify-between lg:justify-around p-6 sm:p-8 md:p-12 lg:p-20 mt-16 sm:mt-16 md:mt-18 lg:mt-20 gap-10 xl:gap-0">
+            <div
+              className="flex-none mb-2 sm:mb-6 md:mb-7 lg:mb-4"
+              data-aos="fade-right"
+            >
+              <Image
+                src={solution2}
+                width={360}
+                height={360}
+                className="w-full sm:w-[360px] md:w-[550px] lg:w-[570px] h-auto"
+                alt="Solutions Overview"
+              />
+            </div>
+            <div
+              className="flex-none max-w-[600px] text-center
           md:text-left
           md:max-w-[470px] lg:max-w-[30rem]"
-            data-aos="fade-left"
-          >
-            <h1
-              className="font-bold text-2xl mb-4 text-black-greyish sm:text-3xl  lg:max-w-[29rem]"
-              style={{ lineHeight: "1.3" }}
+              data-aos="fade-left"
             >
-              Empowering Your Restaurant with Cutting-Edge Solutions
-            </h1>
-            <p className="mb-4 text-[14px] text-black-greyish font-normal sm:text-sm md:text-base lg:text-[14px] lg:text-left leading-snug sm:leading-normal lg:max-w-[22rem]">
-              Clear Eats is the Go-to platform for Restaurants Looking to
-              streamline their operations and elevate the customer experience.
-              Our innovative tools make it easy to manage orders, track sales,
-              and customize your offerings.
-            </p>
-            <div className="flex justify-center lg:justify-start">
-              <button className="bg-purple text-white rounded-full font-bold text-xs p-4">
-                See How Clear Eats works!
-              </button>
+              <h1
+                className="font-bold text-2xl mb-4 text-black-greyish sm:text-3xl  lg:max-w-[29rem]"
+                style={{ lineHeight: "1.3" }}
+              >
+                Empowering Your Restaurant with Cutting-Edge Solutions
+              </h1>
+              <p className="mb-4 text-[14px] text-black-greyish font-normal sm:text-sm md:text-base lg:text-[14px] lg:text-left leading-snug sm:leading-normal lg:max-w-[22rem]">
+                Clear Eats is the Go-to platform for Restaurants Looking to
+                streamline their operations and elevate the customer experience.
+                Our innovative tools make it easy to manage orders, track sales,
+                and customize your offerings.
+              </p>
+              <div className="flex justify-center lg:justify-start">
+                <button className="bg-purple text-white rounded-full font-bold text-xs p-4">
+                  See How Clear Eats works!
+                </button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </section>
     </div>
   )
 }
